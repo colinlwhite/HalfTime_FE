c57deec4ddae807b6903895ad560706ed2f3cebe
Colin White <clwhite814@gmail.com>
2019-08-01 11:56:17 -0500

    wip

--- src/App/App.js
+++ src/App/App.js
@@ -1,37 +1,31 @@
-import React from 'react';
-import { Button } from 'reactstrap';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import authRequests from '../helpers/data/authRequests';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button className='btn btn-danger'>HELP ME</button>
-        <Button
-          tag="a"
-          color="success"
-          size="large"
-          href="http://reactstrap.github.io"
-          target="_blank"
->
-    View Reactstrap Docs
-</Button>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    userObject: {},
+  }
+
+  componentDidMount() {
+    authRequests.getUser()
+      .then((user) => {
+        console.log(user);
+        this.setState({
+          userObject: user,
+        });
+      })
+      .catch(err => ('err'));
+  }
+
+  render() {
+    //  const { userObject } = this.state;
+    return (
+      <div>
+        <h1>HalfTime</h1>
+      </div>
+    );
+  }
 }
 
 export default App;

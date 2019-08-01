cbb7bde9b7368f05224048596ba38584e0c4de37
Colin White <clwhite814@gmail.com>
2019-08-01 17:24:16 -0500

    wip

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {
+  BrowserRouter, Redirect, Route, Switch,
+} from 'react-router-dom';
 import authRequests from '../helpers/data/authRequests';
 import AppNavbar from '../components/AppNavbar/AppNavbar';
 import './App.scss';
 
-class App extends Component {
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = props => (authed === false
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/home', state: { from: props.location } }} />));
+  return <Route {...rest} render={props => routeChecker(props)} />;
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = props => (authed === true
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/auth', state: { from: props.location } }} />));
+  return <Route {...rest} render={props => routeChecker(props)} />;
+};
+
+class App extends React.Component {
   state = {
     userObject: {},
+    authed: false,
   }
 
   componentDidMount() {
@@ -14,17 +32,31 @@ class App extends Component {
         console.log(user);
         this.setState({
           userObject: user,
+          authed: true,
         });
       })
       .catch(err => ('err'));
   }
 
+  componentWillUnmount() {
+    this.setState({
+      authed: false,
+    });
+  }
+
   render() {
     //  const { userObject } = this.state;
     return (
-      <div>
-        <AppNavbar />
-
+      <div className="App">
+        <BrowserRouter>
+          <React.Fragment>
+            <AppNavbar />
+            <div>
+              <Switch>
+              </Switch>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
       </div>
     );
   }

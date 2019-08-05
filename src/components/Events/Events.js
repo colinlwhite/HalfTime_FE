9d1e4f5a50d280bd17d34faa892e4f15b9f454f7
Colin White <clwhite814@gmail.com>
2019-08-05 09:57:22 -0500

    basic events CRUD working

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -52,7 +52,9 @@ class Events extends React.Component {
     return (
       <div>
         <NavLink tag={RRNavLink} to='/eventadd'><button className="btn btn-light">Add Event</button></NavLink>
-        <span><button className="btn btn-primary" onClick={this.sendTextMessage}>SEND NEXT EVENT REMINDER</button></span>
+        <span><button className="btn btn-primary" onClick={this.sendTextMessage}>
+          SEND NEXT EVENT REMINDER
+          </button></span>
         <h1>Events</h1>
         <h3>{eventItemComponents}</h3>
       </div>

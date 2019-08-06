92008f8c7a2244bd2a5642ef9fd52370455e65e0
Colin White <clwhite814@gmail.com>
2019-08-06 17:34:01 -0500

    routing is working on the homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,39 +1,61 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './Home.scss';
 
 class Home extends React.Component {
+  changeView = (e) => {
+    // current target goes straight to where the onClick function is
+    const view = e.currentTarget.id;
+    // const view = e.target.closest('.card').id;
+    this.props.history.push(`/${view}`);
+  }
+
   render() {
     return (
-      <div className="w-100 mx-auto">
-      <div className="d-flex justify-content-center flex-wrap mt-5">
-          <div className="card m-3 border-dark animated zoomIn" id="myGarage" onClick={this.changeView}>
-              <div className="card-body home text-center">
-              <h4 className="card-title"><i class="fas fa-user-friends fa-7x"></i></h4>
-              <h5 className="card-subtitle mb-2 text-muted">Students</h5>
-              </div>
+      <div className='Home mx-auto'>
+      <div className="card-deck">
+        <div className="card border-dark" id="instruments" onClick={this.changeView}>
+          <div className="card-body text-center">
+            <h4 className="card-title"><i className="fas fa-comments fa-7x"></i></h4>
+            <h6 className="card-subtitle mb-2 text-muted">Instruments</h6>
           </div>
-          <div className="card m-3 border-dark animated zoomIn" id='addMacine' onClick={this.toggleMachineModal}>
-              <div className="card-body home text-center">
-              <h4 className="card-title"></h4>
-              <h5 className="card-subtitle mb-2 text-muted">Instruments</h5>
-              </div>
+        </div>
+        <div className="card border-dark" id='students' onClick={this.changeView}>
+          <div className="card-body text-center">
+            <h4 className="card-title"><i className="fas fa-user-friends fa-7x"></i></h4>
+            <h6 className="card-subtitle mb-2 text-muted">Students</h6>
           </div>
-          <div className="card m-3 border-dark animated zoomIn" id='links' onClick={this.changeView}>
-              <div className="card-body home text-center">
-              <h4 className="card-title"><i class="fas fa-tshirt fa-7x"></i></h4>
-              <h5 className="card-subtitle mb-2 text-muted">Uniforms</h5>
-              </div>
+        </div>
+        <div className="card border-dark" id='uniforms' onClick={this.changeView}>
+          <div className="card-body text-center">
+            <h4 className="card-title"><i className="fas fa-sun fa-7x"></i></h4>
+            <h6 className="card-subtitle mb-2 text-muted">Uniforms</h6>
           </div>
-          <div className="card m-3 border-dark animated zoomIn" id='links' onClick={this.changeView}>
-              <div className="card-body home text-center">
-              <h4 className="card-title"><i class="fas fa-calendar-alt fa-7x"></i></h4>
-              <h5 className="card-subtitle mb-2 text-muted">Events</h5>
-              </div>
-          </div>
+        </div>
       </div>
+      <div className="card-deck">
+        <div className="card border-dark" id='events' onClick={this.changeView}>
+          <div className="card-body text-center">
+            <h4 className="card-title"><i className="fas fa-calendar-alt fa-7x"></i></h4>
+            <h6 className="card-subtitle mb-2 text-muted">Events</h6>
+          </div>
+        </div>
+        <div className="card border-dark bg-warning">
+          <div className="card-body text-center">
+            <h4 className="card-title"><i className="fas fa-tree fa-7x"></i></h4>
+            <h6 className="card-subtitle mb-2 text-muted">HalfTime</h6>
+          </div>
+        </div>
+        <div className="card border-dark" id='volunteers' onClick={this.changeView}>
+          <div className="card-body text-center">
+            <h4 className="card-title"><i className="fas fa-newspaper fa-7x"></i></h4>
+            <h6 className="card-subtitle mb-2 text-muted">Volunteers</h6>
+          </div>
+        </div>
       </div>
+    </div>
     );
   }
 }
 
-export default Home;
+export default withRouter(Home);
